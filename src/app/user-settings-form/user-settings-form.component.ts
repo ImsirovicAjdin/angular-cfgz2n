667c0d69963bea49c9cbeba15529173417a4fbf8
ImsirovicAjdin <ajdika@live.com>
2019-07-19 05:10:47 +0200

    Setup the app for example that follows

---
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms'; 
+import { DataService } from '../data/data.service';
+
+@Component({
+  selector: 'app-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styleUrls: [ './user-settings-form.component.css']
+})
+export class UserSettingsFormComponent implements OnInit {
+
+  originalUserSettings : UserSettings = {
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null,
+    password: null,
+    testing: null    
+  }
+
+  userSettings : UserSettings = { ...this.originalUserSettings};
+  postError = false;
+  postErrorMessage = '';
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() { }
+
+  onHttpError(errorResponse: any) {
+    error => console.log('error: ', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit', form.valid);
+
+    if(form.valid) {
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+        result => console.log('success: ', result),
+        error => console.log('error: ', error)
+      )
+    } else {
+      this.postError = true;
+      this.postErrorMessage = "Please fix the above errors";
+    }
+    
+  }
+
+  onBlur(field: NgModel) {
+    console.log('in onBlur', field.valid);
+  }
+
+}
